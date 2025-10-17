f6c2d9270775b4582781267d5ab646c316e80cc8
Varrience <105261606+Varrience@users.noreply.github.com>
2025-10-17 05:42:51 -0400

    add redundancy files & script update
    - add both libraries as a backup source in case i need them later and cloud flare decides they no longer wish to host them
    - updated script to default to a better web viewable experience than a printable one

--- wiki-backup.js
+++ wiki-backup.js
@@ -37,34 +37,56 @@ function makePageBuffers(index) {
         .then(puppet => {
             const doc = puppet.document;
             const win = puppet.window;
+            // set custom dimensions using wxh [] or use 'b4' for best printing
+            // also add [10, 0, 10, 0] so that there are page margins
+            const format = [win.innerWidth, doc.body.scrollHeight];
+            doc.querySelector("html").style['scrollbar-width'] = "none";
             doc.body.querySelector(".vector-page-toolbar").remove();
+            doc.body.querySelector(".mw-body-header > nav:nth-child(1)")?.remove();
             pdfSrc = doc.createElement("script");
             pdfSrc.src = "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js";
             pdfSrc.addEventListener("load", () => {
-                win.html2pdf(doc.body.querySelector(".mw-page-container"))
-                    .outputPdf("blob")
-                    .then(data => {
-                        archiveBlobs.push({ title: encodeURIComponent(doc.title) + ".pdf", data: data });
-                        if (index < archiveList.length - 1) {
-                            makePageBuffers(++index);
-                        } else {
-                            let zip = new JSZip();
-                            for (let blob of archiveBlobs) {
-                                zip.file(blob.title, blob.data);
-                            }
-                            zip.generateAsync({ type: "blob" })
-                                .then(mime => {
-                                    const file = document.createElement("a");
-                                    const url = URL.createObjectURL(mime);
-                                    file.download = "Archive_" + Date().slice(0, 33);
-                                    file.href = url;
-                                    file.style.display = "hidden"
-                                    document.body.appendChild(file);
-                                    window.open(url);
-                                })
+                win.gotBlob = function (blob) {
+                    archiveBlobs.push({ title: encodeURIComponent(doc.title) + ".pdf", data: blob });
+                    if (index < archiveList.length - 1) {
+                        makePageBuffers(++index);
+                    } else {
+                        let zip = new JSZip();
+                        for (let blob of archiveBlobs) {
+                            zip.file(blob.title, blob.data);
                         }
-                        win.close();
+                        zip.generateAsync({ type: "blob" })
+                            .then(mime => {
+                                const file = document.createElement("a");
+                                const url = URL.createObjectURL(mime);
+                                file.download = "CRW_" + Date().slice(0, 33);
+                                file.href = url;
+                                file.style.display = "hidden"
+                                document.body.appendChild(file);
+                                file.click();
+                                document.body.removeChild(file);
+                            })
+                    }
+                    win.close();
+                }
+                let src = doc.createElement("script");
+                src.innerHTML = `
+                const worker = html2pdf();
+                worker.set({
+                    margin: [0, 0, 0, 0], //top, left, buttom, right,
+                    image: { type: 'jpeg', quality: 0.7 },
+                    html2canvas: { dpi: 192, letterRendering: true },
+                    jsPDF: { unit: 'px', format: [${format[0]}, ${format[1]}], orientation: 'portrait' },
+                    pagebreak: { mode: 'avoid-all' }
+                })
+                    .from(document.body.querySelector(".mw-page-container"))
+                    .toPdf()
+                    .output("blob")
+                    .then(data => {
+                        gotBlob(data);
                     });
+                `
+                doc.body.appendChild(src);
             });
             doc.head.appendChild(pdfSrc);
         })
